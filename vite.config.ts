d4eebe02f7233f5078057282bb044fca8ec0e512
hoanggbao00 <hoanggbao00@gmail.com>
2024-12-12 19:55:22 +0700

    fix build require not define

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		commonjsOptions: { transformMixedEsModules: true }
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
